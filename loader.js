7096036a1b31806520b92033d0d9fbe10c6c0a01
admin <omccleary03@gmail.com>
2023-03-09 14:53:25 -0500

    add words and scrabble page

---
+++ loader.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/* Add loading transition */
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+  
+    loader.classList.add("loader--hidden");
+  
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
